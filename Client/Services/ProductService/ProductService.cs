cb8b16ffe0f6f6155d883e96b7b6112942fbcea3
Alex Olowookere <alex.oluwasina@firstnational.ca>
2023-07-27 15:26:54 -0400

    Added a search feature to the controller on the backend and implemented a seach box on the frontend

--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -9,6 +9,7 @@
 			_http = http;
 		}
 		public List<Product> Products { get; set; } = new List<Product>();
+		public string Message { get; set; } = "Loading Products...";
 
 		public event Action ProductChanged;
 
@@ -27,6 +28,27 @@
 			if (result != null && result.Data != null)
 				Products = result.Data;
 
+			ProductChanged.Invoke();
+		}
+
+		public async Task<List<string>> GetProductSearchSuggestion(string searchText)
+		{
+			var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+			return result.Data;
+		}
+
+		public async Task SearchProducts(string searchText)
+		{
+			var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+
+			if(result !=null && result.Data != null)
+			{
+				Products = result.Data;
+			}
+			if (Products.Count == 0)
+			{
+				Message = "No products found :(";
+			}
 			ProductChanged.Invoke();
 		}
 	}
