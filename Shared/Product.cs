b78f0109ef9eada869944ea3ee1d2b8711725415
Alex Olowookere <alex.oluwasina@firstnational.ca>
2023-07-31 13:28:26 -0400

    Added a home button feature and changed the layout of the navigation menu

--- Shared/Product.cs
+++ Shared/Product.cs
@@ -17,6 +17,8 @@ namespace BlazorEcommerce.Shared
 
 		public int CategoryId { get; set; }
 
+		public bool featured { get; set; } = false;
+
 		public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
 	}
 }
